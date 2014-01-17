7a1d393eb8e100981c7afedff8cfeaca918e92db
groDAs76 <dngros@live.com>
2014-01-17 10:07:53 -0600

    redid last commit with add

---
+++ KingdomDart/web/FileMapGenerator.dart
@@ -0,0 +1,18 @@
+part of Kingdom;
+
+class FileMapGenerator implements MapLoader{
+  int _mapWidth;
+  int _mapHeight;
+  List<int> _data;
+  FileMapGenerator(String filename){
+    
+  }
+  List<int> getData(){
+    
+  }
+  int get width => _mapWidth;
+  int get height => _mapHeight;
+  
+  //int _stringToInt(const std::string& str);
+  
+}
